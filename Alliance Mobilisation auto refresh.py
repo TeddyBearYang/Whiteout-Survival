772c5d54ca4c4cc0252fa68abb3932aff3e48b5b
PeterYeh0176 <peteryeh160@gmail.com>
2024-07-29 19:04:39 +1000

    updated images

--- Alliance Mobilisation auto refresh.py
+++ Alliance Mobilisation auto refresh.py
@@ -6,82 +6,111 @@ import time
 import mss
 import pyautogui as pg
 
+# Set a threshold to identify matching regions and refresh time
+threshold = 0.98
+refresh = 300
+
+# Choose which side of mission to enable auto refresh
+left = 1
+right = 0
+
 # File path
 os.chdir('D:\GitHub\Whiteout-Survival')
 
 # Load mission images
 mission_1 = cv2.imread('target_mission_1.png', cv2.IMREAD_GRAYSCALE)
 mission_2 = cv2.imread('target_mission_2.png', cv2.IMREAD_GRAYSCALE)
-mission_3 = cv2.imread('target_mission_3.png', cv2.IMREAD_GRAYSCALE)
 
-# Initialise mission location on screen
-exclusive_mission_monitor_left = {'left': 850,
+# Initialise left mission location on screen
+exclusive_mission_monitor_left = {'left': 1335,
         'top': 490,
         'width': 195,
         'height': 220
         }
 
-# Initialise mission location on screen
-exclusive_mission_monitor_right = {'left': 1015,
+# Initialise right mission location on screen
+exclusive_mission_monitor_right = {'left': 1500,
         'top': 490,
         'width': 195,
         'height': 220
         }
+
+# Initialise loop counter
+i = 1
 
 # Create an MSS instance
 with mss.mss() as sct:
     while True:
-        # Capture the screen
-        screen = sct.grab(exclusive_mission_monitor_left)
+        ############# Left mission #################
+        # Check if left side is enabled
+        if left == 1:
         
-        # Convert the screen to a NumPy array
-        img = np.array(screen)
-        
-        # Convert the screen to grayscale
-        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        
-        # Perform template matching
-        result1 = cv2.matchTemplate(img_gray, mission_1, cv2.TM_CCOEFF_NORMED)
-        result2 = cv2.matchTemplate(img_gray, mission_2, cv2.TM_CCOEFF_NORMED)
-        result3 = cv2.matchTemplate(img_gray, mission_3, cv2.TM_CCOEFF_NORMED)
-
-        # Set a threshold to identify matching regions
-        threshold = 0.85
-        
-        # Check if the target mission has spawned
-        if result1.max() < threshold and result2.max() < threshold and result3.max() < threshold:            
-            # Refresh left mission
-            pg.moveTo(947,580,0.2)
-            pg.click(947,580)
-            pg.moveTo(1029,677,0.5)
-            pg.click()
-            pg.moveTo(1193,686,0.5)
-            pg.click()
+            # Capture the screen
+            screen = sct.grab(exclusive_mission_monitor_left)
             
-        #######################################################################
-        # Capture the screen
-        screen = sct.grab(exclusive_mission_monitor_right)
+            # Convert the screen to a NumPy array
+            img = np.array(screen)
+            
+            # Convert the screen to grayscale
+            img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            
+            # Perform template matching
+            result1 = cv2.matchTemplate(img_gray, mission_1, cv2.TM_CCOEFF_NORMED)
+            result2 = cv2.matchTemplate(img_gray, mission_2, cv2.TM_CCOEFF_NORMED)
+            
+            # Check if the target mission has spawned
+            if result1.max() < threshold and result2.max() < threshold:           
+                # Refresh left mission
+                pg.moveTo(1460,580,0.2)
+                pg.click(1460,580)
+                pg.moveTo(1515,677,0.2)
+                pg.click()
+                pg.moveTo(1690,657,0.2)
+                pg.click()
+                
+            # Save image and print similarity score
+            cv2.imwrite('left_mission_image'+str(i)+'.png', img)
+            print('Left mission ' + str(i) + ' similarity score with target mission 1 & 2 is: '
+                  + str(round(result1.max(),3)) + ' and ' + str(round(result2.max(),3)))
+            
+            time.sleep(1)
         
-        # Convert the screen to a NumPy array
-        img = np.array(screen)
+        ################ Right mission ###############################################
+        # Check if right side is enabled
+        if right == 1:
         
-        # Convert the screen to grayscale
-        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        
-        # Perform template matching
-        result1 = cv2.matchTemplate(img_gray, mission_1, cv2.TM_CCOEFF_NORMED)
-        result2 = cv2.matchTemplate(img_gray, mission_2, cv2.TM_CCOEFF_NORMED)
-        result3 = cv2.matchTemplate(img_gray, mission_3, cv2.TM_CCOEFF_NORMED)
-        
-        if result1.max() < threshold and result2.max() < threshold and result3.max() < threshold:
-            # Refresh right mission
-            pg.moveTo(1109,580,0.2)
-            pg.click(1109,580)
-            pg.moveTo(1029,677,0.5)
-            pg.click()
-            pg.moveTo(1193,686,0.5)
-            pg.click()
+            # Capture the screen
+            screen = sct.grab(exclusive_mission_monitor_right)
+    
+            # Convert the screen to a NumPy array
+            img = np.array(screen)
+    
+            # Convert the screen to grayscale
+            img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+            # Perform template matching
+            result1 = cv2.matchTemplate(img_gray, mission_1, cv2.TM_CCOEFF_NORMED)
+            result2 = cv2.matchTemplate(img_gray, mission_2, cv2.TM_CCOEFF_NORMED)
+            # result3 = cv2.matchTemplate(img_gray, mission_3, cv2.TM_CCOEFF_NORMED)
+            
+            if result1.max() < threshold and result2.max() < threshold:
+                # Refresh right mission
+                pg.moveTo(1609,580,0.2)
+                pg.click(1609,580)
+                pg.moveTo(1515,677,0.2)
+                pg.click()
+                pg.moveTo(1690,657,0.2)
+                time.sleep(1)
+                pg.click()
+            
+            # Save image
+            cv2.imwrite('right_mission_image'+str(i)+'.png', img)
+            print('Right mission ' + str(i) + ' similarity score with target mission 1 & 2 is: '
+                  + str(round(result1.max(),3)) + ' and ' + str(round(result2.max(),3)))
+            
+        # Update counter
+        i += 1
                 
         # Wait for specified time before the next capture
-        time.sleep(302)
+        time.sleep(refresh + 1)
 
--- Auto heal.py
+++ Auto heal.py
@@ -7,31 +7,31 @@ Created on Thu Jun 27 22:46:56 2024
 
 import cv2
 import numpy as np
-import mss
 import os
 import time
-import mss.tools
+import mss
+import pyautogui as pg
 
 # File path
 os.chdir('D:\GitHub\Whiteout-Survival')
 
 # Load the template image
-template = cv2.imread('target_mission_2.png', cv2.IMREAD_GRAYSCALE)
+template = cv2.imread('help_button.png', cv2.IMREAD_GRAYSCALE)
 w, h = template.shape[::-1]
 
 # Define the screen capture region (adjust this to your game window's position and size)
-monitor = {'left': 1335,
-        'top': 490,
-        'width': 195,
-        'height': 220
-        }
-
-# Right box
-# monitor = {'left': 1500,
+# monitor = {'left': 850,
 #         'top': 490,
 #         'width': 195,
 #         'height': 220
 #         }
+
+# Right box
+monitor = {'left': 1615,
+        'top': 790,
+        'width': 195,
+        'height': 220
+        }
 
 # Create an MSS instance
 with mss.mss() as sct:
@@ -49,19 +49,15 @@ with mss.mss() as sct:
         result = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
         
         # Set a threshold to identify matching regions
-        threshold = 0.96
+        threshold = 0.88
         locations = np.where(result >= threshold)
         
-        # Draw rectangles around the matched regions
-        for pt in zip(*locations[::-1]):
-            cv2.rectangle(img, pt, (pt[0] + w, pt[1] + h), (0, 255, 0), 2)
         
-        # Display the result
-        cv2.imshow('Detected', img)
-        
-        # Break the loop if 'q' is pressed
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+        if result.max() >= threshold:
+            # Click help
+            pg.moveTo(1710,910,0.1)
+            pg.click()
+            
         time.sleep(1)
 
 # Release resources
